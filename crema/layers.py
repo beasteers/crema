491da9bf2edd9890a00749e29ddb27962b938010
Bea Steers <bsteers@nyu.edu>
2020-07-20 18:47:14 -0400

    convert to tf.keras

--- crema/layers.py
+++ crema/layers.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 '''Custom Keras layers'''
 
-import keras.backend as K
-from keras.engine.topology import Layer
+import tensorflow.keras.backend as K
+from tensorflow.keras.layers import Layer
 
 
 __all__ = ['SqueezeLayer']
@@ -12,25 +12,17 @@ class SqueezeLayer(Layer):
     '''
     Keras squeeze layer: remove a length=1 axis
     '''
+
     def __init__(self, axis=-1, **kwargs):
         super(SqueezeLayer, self).__init__(**kwargs)
         self.axis = axis
 
-    def get_output_shape_for(self, input_shape):
-        # shape = np.array(input_shape)
-        # shape = shape[shape != 1]
-        # return tuple(shape)
-        shape = list(input_shape)
-        del shape[self.axis]
-        return tuple(shape)
-
     def compute_output_shape(self, input_shape):
-        return self.get_output_shape_for(input_shape)
+        return (tuple(input_shape[:self.axis])
+                + tuple(input_shape[self.axis+1:]))
 
     def call(self, x, mask=None):
         return K.squeeze(x, axis=self.axis)
 
     def get_config(self):
-        config = {'axis': self.axis}
-        base_config = super(SqueezeLayer, self).get_config()
-        return dict(list(base_config.items()) + list(config.items()))
+        return dict(super(SqueezeLayer, self).get_config(), axis=self.axis)
